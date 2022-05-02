4a6cb6ab8a13e1833dd231706f7326e681a282d5
pranto <polasahammedpranto@gmail.com>
2022-05-02 13:51:43 +0600

    mongodb crud operations and data insert add

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
 
-
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y44yk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run () {
+    try{
+        await client.connect();
+        const productCollection = client.db("groceryStock").collection("product");
+        console.log('db connected');
+        app.get('/products', async (req, res) => {
+            const quary= {}
+            const cursor = productCollection.find(quary)
+            const products = await cursor.toArray()
+            res.send(products)
+        });
+        
+    }
+    finally{
+        // client.close()
+    }
+}
+run().catch(console.dir);
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('running grocery server');
